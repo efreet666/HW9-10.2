fab38b39d1d3e59955d3e08f392abaecfaec7f20
Vlad <vladislav.bok@yandex.ru>
2021-08-26 21:17:09 +0300

    Initial Commit

---
+++ HW9-10.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW9-10.2
+//
+//  Created by Влад Бокин on 26.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
